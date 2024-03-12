0ebbf4e304f416255df36dcdd23e459ff513148e
shiesse <vago0915@gmail.com>
2024-03-12 19:43:53 +0900

    Moved classes

--- expertise/importstandart/models.py
+++ expertise/importstandart/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class Speciality:
+    def __init__(self,CodePS,FullCode,Name,RegNumber,Description,OccupationGroup,EconomicActivity):
+        self.CodePS=CodePS
+        self.FullCode=FullCode
+        self.Name=Name
+        self.RegNumber=RegNumber
+        self.Description=Description
+        self.OccupationGroup=OccupationGroup
+        self.EconomicActivity=EconomicActivity
+
+    def info(self):
+        print("Код ПС:", self.CodePS)
+        print("Полный Код", self.FullCode)
+        print("Наименование", self.Name)
+        print("Регистрационный номер", self.RegNumber)
+        print("Основная цель вида профессиональной деятельности", self.Description)
+        print("Группа занятий:")
+        for group in self.OccupationGroup:
+            print("Код ОКЗ:", group["CodeOKZ"])
+            print("Наименование:", group["Name"])
+        print("Отнесение к видам экономической деятельности:")
+        for activity in self.EconomicActivity:
+            print("Код ОКВЭД:", activity["CodeOKVED"])
+            print("Наименование:", activity["Name"])
+
+
+testData = {
+    "CodePS": "06",
+    "FullCode": "06.001",
+    "Name": "Программист",
+    "RegNumber": "4",
+    "Description": "Разработка, отладка, проверка работоспособности, модификация компьютерного программного обеспечения",
+    "OccupationGroup": [
+        {"CodeOKZ": "3512", "Name": "Специалисты-техники по поддержке пользователей ИКТ"},
+        {"CodeOKZ": "2512", "Name": "Разработчики программного обеспечения"},
+        {"CodeOKZ": "2514", "Name": "Программисты приложений"}
+    ],
+    "EconomicActivity": [
+        {"CodeOKVED": "62.01", "Name": "Разработка компьютерного программного обеспечения"}
+    ]
+}
+
+test = Speciality(**testData)
+test.info()
--- expertise/importword/models.py
+++ expertise/importword/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class TheTrainingProgram:
+    def __init__(self, name, duration, instructor, materials):
+        self.name = name  # Название программы обучения
+        self.duration = duration  # Продолжительность программы
+        self.instructor = instructor  # Преподаватель
+        self.materials = materials  # Материалы для обучения
+
+    def start_program(self):
+        # Метод для начала программы обучения
+        print(f"Программа обучения '{self.name}' началась.")
+        print(f"Продолжительность: {self.duration}.")
+        print(f"Инструктор: {self.instructor}.")
+
+    def complete_program(self):
+        # Метод для завершения программы обучения
+        print(f"Программа обучения '{self.name}' завершена.")
+
+    def get_materials(self):
+        # Метод для получения материалов программы обучения
+        print(f"Материалы для программы обучения '{self.name}':")
+        for material in self.materials:
+            print(material)
