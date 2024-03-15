2042c077890b590011a2daa24659280acc6cbe8b
shiesse <138016456+shiesse@users.noreply.github.com>
2024-03-15 22:51:20 +0900

    Update models.py

--- expertise/importword/models.py
+++ expertise/importword/models.py
@@ -1,12 +1,17 @@
-from django.db.models import Model, CharField, IntegerField
+from django.db import models
 
-# Create your models here.
-class TrainingProgram(Model):
-    number = CharField("Номер", max_length=10, db_index=True)
-    name = CharField("Наименование", max_length=300, db_index=True)  # Название программы обучения
-    brief_name = CharField("Краткое наименование", max_length=10)
-    duration = duration  # Продолжительность программы
-    instructor = instructor  # Преподаватель
-    materials = materials  # Материалы для обучения
+class TrainingProgram(models.Model):
+    number = models.CharField("Номер", max_length=10, db_index=True)
+    name = models.CharField("Наименование", max_length=300, db_index=True)  # Название программы обучения
+    brief_name = models.CharField("Краткое наименование", max_length=10)
+    duration = models.DurationField()  # Продолжительность программы
+    instructor = models.ForeignKey('Instructor', on_delete=models.CASCADE)  # Преподаватель
+    materials = models.ManyToManyField('Material')  # Материалы для обучения
+
+class Instructor(models.Model):
+    name = models.CharField("Имя", max_length=100)
 
 
+class Material(models.Model):
+    title = models.CharField("Название", max_length=200)
+    description = models.TextField("Описание", blank=True, null=True)
