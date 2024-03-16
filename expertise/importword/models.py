7d8d785eccd55ca49cbf6e166e8cc73e854b2b40
shiesse <138016456+shiesse@users.noreply.github.com>
2024-03-16 13:40:12 +0900

    Update models.py

--- expertise/importword/models.py
+++ expertise/importword/models.py
@@ -1,17 +1,25 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+spo = 0
+bachalour = 1
+master = 2
+specialist = 3
+EDU_LEVEL = ((spo, "Среденнее образование"), (bachalour, "Бакалавриат"), (master, "Магистратура"), (specialist,"Специалитет"))
+
+russian = 0
+english = 1
+EDU_LANG = ((russian, "Русский"),(english, "Английский"))
 
 class TrainingProgram(models.Model):
     number = models.CharField("Номер", max_length=10, db_index=True)
     name = models.CharField("Наименование", max_length=300, db_index=True)  # Название программы обучения
     brief_name = models.CharField("Краткое наименование", max_length=10)
-    duration = models.DurationField()  # Продолжительность программы
-    instructor = models.ForeignKey('Instructor', on_delete=models.CASCADE)  # Преподаватель
-    materials = models.ManyToManyField('Material')  # Материалы для обучения
+    duration = models.IntegerField("Продолжительность", validators=[MinValueValidator(1), MaxValueValidator(6)])  # Продолжительность программы
+    level = models.IntegerField("Уровень образования", choices=EDU_LEVEL)
+    language = models.IntegerField("Язык обучения", choices=EDU_LANG)
+    description = models.TextField("Описание")
+    zet = models.IntegerField("ЗЕТ", validators=[MinValueValidator(1), MaxValueValidator(361)])
+    
 
-class Instructor(models.Model):
-    name = models.CharField("Имя", max_length=100)
-
-
-class Material(models.Model):
-    title = models.CharField("Название", max_length=200)
-    description = models.TextField("Описание", blank=True, null=True)
