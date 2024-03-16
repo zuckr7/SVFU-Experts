6664b01231f1997aa001321f5ea5a5956d12a74a
zuckr7 <realslimshady30@gmail.com>
2024-03-16 14:35:03 +0900

    Migratoins

--- expertise/importstandart/admin.py
+++ expertise/importstandart/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import OccupationGroup, EconomicActivity, Speciality
 # Register your models here.
+
+admin.site.register(OccupationGroup)
+admin.site.register(EconomicActivity)
+admin.site.register(Speciality)
