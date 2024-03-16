78b320d231abebafa8e1cd35b97c3578496b932a
zuckr7 <realslimshady30@gmail.com>
2024-03-16 12:30:21 +0900

    Update models.py

--- expertise/importstandart/models.py
+++ expertise/importstandart/models.py
@@ -3,10 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Speciality(models.Model):
-    CodePS = models.CharField(max_length=2)
-    FullCode = models.CharField(max_length=6)
-    Name = models.CharField(max_length=300)
-    RegNumber = models.CharField(max_length=20)
-    Description = models.TextField()
-    OccupationGroup = models.JSONField()
-    EconomicActivity = models.JSONField()
+    CodePS = models.CharField("Код ПС",max_length=2)
+    FullCode = models.CharField("Полный Код",max_length=6)
+    Name = models.CharField("Наименование",max_length=300)
+    RegNumber = models.CharField("Регистрационный номер",max_length=20)
+    Description = models.TextField("Основная цель вида профессиональной деятельности")
+    OccupationGroup = models.ManyToManyField("Группа занятий")
+    EconomicActivity = models.ManyToManyField("Виды экономической деятельности")
+
+class OccupationGroup(models.Model):
+    CodeOKZ = models.CharField("Код ОКЗ",max_length=4)
+    Name = models.CharField("Наименование",max_length=300)
+
+class EconomicActivity(models.Model):
+    CodeOKVED = models.CharField("Код ОКВЭД",max_length=10)
+    Name = models.CharField("Наименование",max_length=300)
