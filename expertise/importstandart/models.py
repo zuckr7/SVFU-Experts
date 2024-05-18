e106d8b0a14c3eaa25c807f7707cba14596571db
zuckr7 <realslimshady30@gmail.com>
2024-05-18 11:26:03 +0900

    models?

--- expertise/importstandart/models.py
+++ expertise/importstandart/models.py
@@ -3,35 +3,43 @@ from rest_framework import routers,serializers,viewsets
 # Create your models here.
 
 
-class OccupationGroup(models.Model):
-    CodeOKZ = models.CharField("Код ОКЗ",max_length=4)
-    Name = models.CharField("Наименование",max_length=300)
+# class OccupationGroup(models.Model):
+#     codeOKZ = models.CharField("Код ОКЗ",max_length=4)
+#     name = models.CharField("Наименование",max_length=300)
 
 
-class EconomicActivity(models.Model):
-    CodeOKVED = models.CharField("Код ОКВЭД",max_length=10)
-    Name = models.CharField("Наименование",max_length=300)
+# class EconomicActivity(models.Model):
+#     codeOKVED = models.CharField("Код ОКВЭД",max_length=10)
+#     name = models.CharField("Наименование",max_length=300)
 
 
 class WorkFunction(models.Model):
-    Code = models.CharField()
-    Name = models.CharField()
-    Qualification = models.CharField()
+    code = models.CharField("Код",max_length=50)
+    name = models.CharField("Наименование",max_length=255)
+    qualification = models.CharField("Квалификация",max_length=255)
+    class Meta:
+        abstract = True
 
 
 class GeneralFunction(WorkFunction):
-    PossibleJobs = models.ForeignKey()
+    prof_standart = models.ForeignKey(ProfStandart, related_name='general_functions', on_delete=models.CASCADE)
+    possible_jobs = models.JSONField()
+    edu_requirements = models.JSONField()
+    requirement_experience = models.JSONField()
+    special_conditions = models.JSONField()
 
 
 class ParticularFunction(WorkFunction):
-    pass
+    general_function = models.ForeignKey(GeneralFunction, related_name='particular_functions', on_delete=models.CASCADE)
+    labor_act = models.JSONField()
+    req_skill = models.JSONField()
+    knowledge = models.JSONField()
 
 
 class ProfStandart(models.Model):
-    Title = models.CharField("Наименование",max_length=300)
-    RegNumber = models.CharField("Регистрационный номер",max_length=20)
-    CodePS = models.CharField("Код ПС",max_length=15)
-    Goal = models.TextField("Основная цель вида профессиональной деятельности")
-    OccupationGroup = models.ManyToManyField(OccupationGroup, verbose_name="Группа занятий")
-    EconomicActivity = models.ManyToManyField(EconomicActivity, verbose_name="Виды экономической деятельности")
-    GeneralizedFunctions = models.ManyToManyField(GeneralFunction, verbose_name="Обобщенные трудовые функции")
+    title = models.CharField("Наименование",max_length=300)
+    regNumber = models.CharField("Регистрационный номер",max_length=20)
+    codePS = models.CharField("Код ПС",max_length=15)
+    goal = models.TextField("Основная цель вида профессиональной деятельности")
+    list_okz = models.JSONField("Группа занятий")
+    list_okved = models.JSONField("Отнесение к видам ЭД")
